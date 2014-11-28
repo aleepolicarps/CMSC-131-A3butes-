3ecec6bbf733decd080556e6f072e083fd504b6c
Eva Carandang <carandang_eva@yahoo.com>
2014-11-29 00:17:11 +0800

    Final Assembly to Java
    Includes:
    - do-while loop
    - registers are declared as integers

--- AssemblyToJava.java
+++ AssemblyToJava.java
@@ -3,8 +3,11 @@ package assembler;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class AssemblyToJava {
@@ -14,12 +17,16 @@ public class AssemblyToJava {
 	private ArrayList<String> stringVar = new ArrayList<String>();
 	private ArrayList<String> intVar = new ArrayList<String>();
 	private ArrayList<String> javaMain = new ArrayList<String>();
+	private StringBuilder java = new StringBuilder();
+	String fileName = "";
 	
 	public AssemblyToJava(String fileName){
+		this.fileName = fileName.substring(0, fileName.indexOf("."));
 		loadFile(fileName);
 		getVariables();
 		getMain();
 		analyzeCode();
+		printJavaFile();
 	}
 	private void loadFile(String fileName){
 		File file = new File(fileName);
@@ -30,6 +37,7 @@ public class AssemblyToJava {
 			while(source!=null){
 				source=source.replace("\n", "");
 				source=source.replace("\t", "");
+				source=source.trim();
 				if(!source.equals(""))
 					code.add(source);
 				source = reader.readLine();
@@ -100,6 +108,20 @@ public class AssemblyToJava {
 		code = (ArrayList<String>) mainCode.clone();
 	}
 	private void analyzeCode(){
+		
+		for(String line:code){
+			line = line.trim();
+		}
+		
+		String ifLabel = "";
+		String endIfLabel = "";
+		String endWhileLabel = "";
+		String loopLabel = "";
+		int doIndex = 0;
+		Boolean ifLabelSet=false;
+		Boolean ifEndLabelSet=false;
+		Boolean whileSet = false;
+		
 		for(int i=0;i<code.size();i++){
 			String currLine = code.get(i);
 			//System.out.println(currLine);
@@ -126,14 +148,111 @@ public class AssemblyToJava {
 				javaMain.add(mul());
 			else if(currLine.contains("div "))
 				javaMain.add(div());
+			else if(currLine.contains("cmp ")&&!whileSet){
+				String first = currLine.substring(currLine.indexOf("cmp")+4,currLine.indexOf(","));
+				String second = currLine.substring(currLine.indexOf(",")+1).replace(" ", "");
+				String nextLine = code.get(i+1);
+				String jumpType = nextLine.substring(0,nextLine.indexOf(" "));
+				ifLabel = nextLine.substring(nextLine.indexOf(" ")+1);
+				ifLabelSet = true;
+				String sample = "if("+first+comparator(jumpType)+second+"){";
+				javaMain.add(sample);
+			}
+			else if(whileSet && currLine.contains("cmp") && code.get(i+1).contains(loopLabel)){
+				
+				javaMain.add(doIndex, "do{");
+				String first = currLine.substring(currLine.indexOf("cmp")+4,currLine.indexOf(","));
+				String second = currLine.substring(currLine.indexOf(",")+1).replace(" ", "");
+				String nextLine = code.get(i+1);
+				String jumpType = nextLine.substring(0,nextLine.indexOf(" "));
+				javaMain.add("}while(!("+first+comparator(jumpType)+second+"));");
+				whileSet = false;
+			}
+			else if(whileSet && currLine.contains("cmp")){
+				String first = currLine.substring(currLine.indexOf("cmp")+4,currLine.indexOf(","));
+				String second = currLine.substring(currLine.indexOf(",")+1).replace(" ", "");
+				String nextLine = code.get(i+1);
+				String jumpType = nextLine.substring(0,nextLine.indexOf(" "));
+				String sample = "while("+first+comparator(jumpType)+second+"){";
+				endWhileLabel = nextLine.substring(nextLine.indexOf(" ")+1);
+				javaMain.add(sample);
+			}
+			else if(whileSet && currLine.equals(endWhileLabel+":")){
+				javaMain.add("}");
+				whileSet=false;
+			}
+			else if(ifLabelSet && currLine.contains("jmp")){
+				endIfLabel = currLine.substring(4);
+				ifEndLabelSet = true;
+			}
+			else if(ifLabelSet && currLine.equals(ifLabel+":")){
+				javaMain.add("}");
+				ifLabelSet = false;
+				
+				if(ifEndLabelSet){
+					javaMain.add("else{");
+				}	
+			}
+			else if(ifEndLabelSet && currLine.equals(endIfLabel+":")){
+				ifEndLabelSet = false;
+				javaMain.add("}");
+			}
+			else if(!ifEndLabelSet && !ifLabelSet && currLine.contains(":")){
+				whileSet = true;
+				loopLabel = currLine.substring(0, currLine.indexOf(":"));
+				doIndex = javaMain.size();
+			}
 		}
-		for(String line:javaMain)
-			System.out.println(line);
 	}
 	
-	
-	
-	
+	private void printJavaFile(){
+		fileName = fileName.substring(0,1).toUpperCase() + fileName.substring(1);
+		java.append("public class "+fileName+ " {\n");
+		java.append("public static void main(String[] args){\n");
+		
+		for(int i = 0; i< intVar.size();i++){
+			java.append("int "+ intVar.get(i) + " = "+intVar.get(++i)+";\n");
+		}
+		for(int i = 0; i< stringVar.size();i++){
+			java.append("String "+ stringVar.get(i) + " = "+stringVar.get(++i)+";\n");
+		}
+		
+		java.append("int ax = 0;\n");
+		java.append("int bx = 0;\n");
+		java.append("int cx = 0;\n");
+		java.append("int dx = 0;\n");
+		java.append("int al = 0;\n");
+		java.append("int ah = 0;\n");
+		java.append("int bl = 0;\n");
+		java.append("int bh = 0;\n");
+		java.append("int cl = 0;\n");
+		java.append("int ch = 0;\n");
+		java.append("int dl = 0;\n");
+		java.append("int dh = 0;\n");
+		
+		
+		for(String line:javaMain){
+			java.append(line + "\n");
+		}
+		
+		java.append("}\n}");
+		
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter outputStream = null;
+		
+		try
+		{
+			outputStream = new PrintWriter(new FileOutputStream(fileName+".java"));
+			outputStream.println(java.toString());
+			outputStream.close();
+		}
+		catch(FileNotFoundException e){
+			System.out.println("File not found.");
+		}
+		catch(IOException e){
+			System.out.println("Error");
+		}	
+	}
 	
 	private String inc(String line){
 		line = line.substring(4);
@@ -192,6 +311,17 @@ public class AssemblyToJava {
 		line = "al="+firstLine+"*"+secondLine+";\n";
 		line = line + "ah="+firstLine+"%"+secondLine+";"; 
 		return line;
+	}
+	private String comparator(String line){
+		switch(line){
+		case "jne": return "==";
+		case "je": return "!=";
+		case "jl": return ">=";
+		case "jg": return "<=";
+		case "jle": return ">";
+		case "jge": return "<";
+		}
+		return "";
 	}
 	
 }
